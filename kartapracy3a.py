54cabedd0447aea633c79bd66f3432889fef0179
RS31 <114557710+RadomirSz@users.noreply.github.com>
2022-10-19 06:08:26 +0000

    zad 1 i 2

--- kartapracy3a.py
+++ kartapracy3a.py
@@ -1,5 +1,13 @@
 #zad1
-n = int(input())
-for i in range(n):
-  print("*-|", end=" ")
+# n = int(input())
+# for i in range(n):
+#   print("*-|", end=" ")
 #zad2
+n = int(input())
+for i in range(1, n+1):
+  print("*"*i,end="")
+  if i%2:
+    print("||",end="")
+  else:
+    print("--",end="")
+#zad3
